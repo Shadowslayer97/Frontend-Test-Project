06cff4536f789bf6481469df6042a0ea4aaa1b54
Srirang97 <srirangsathyan17@gmail.com>
2018-11-28 17:29:35 +0530

    fix: category individual component

--- client-app/src/app/authentication/authentication.component.ts
+++ client-app/src/app/authentication/authentication.component.ts
@@ -24,41 +24,6 @@ export class AuthenticationComponent implements OnInit {
     }
   }
 
-  ngAfterViewInit(){
-          $(document).ready(function(){
-            $(".btn").click(function() {
-              $(".form-signin").toggleClass("form-signin-left");
-              $(".form-signup").toggleClass("form-signup-left");
-              $(".frame").toggleClass("frame-long");
-              $(".signup-inactive").toggleClass("signup-active");
-              $(".signin-active").toggleClass("signin-inactive");
-              $(".forgot").toggleClass("forgot-left");
-              $(this).removeClass("idle").addClass("active");
-              });
-            });
-
-            $(function() {
-              $(".btn-signup").click(function(e) {
-                $(".nav").toggleClass("nav-up");
-                $(".form-signup-left").toggleClass("form-signup-down");
-                $(".success").toggleClass("success-left");
-                $(".frame").toggleClass("frame-short");
-              });
-            });
-
-            $(function() {
-              $(".btn-signin").click(function() {
-                $(".btn-animate").toggleClass("btn-animate-grow");
-                $(".welcome").toggleClass("welcome-left");
-                $(".cover-photo").toggleClass("cover-photo-down");
-                $(".frame").toggleClass("frame-short");
-                $(".profile-photo").toggleClass("profile-photo-down");
-                $(".btn-goback").toggleClass("btn-goback-up");
-                $(".forgot").toggleClass("forgot-fade");
-              });
-            });
-    }
-
     signUpUser() {
       console.log(this.userObject);
       if(this.confirmPassword === this.userObject.password) {
@@ -82,5 +47,40 @@ export class AuthenticationComponent implements OnInit {
             console.log(error);
           })
       }
+
+      ngAfterViewInit(){
+              $(document).ready(function(){
+                $(".btn").click(function() {
+                  $(".form-signin").toggleClass("form-signin-left");
+                  $(".form-signup").toggleClass("form-signup-left");
+                  $(".frame").toggleClass("frame-long");
+                  $(".signup-inactive").toggleClass("signup-active");
+                  $(".signin-active").toggleClass("signin-inactive");
+                  $(".forgot").toggleClass("forgot-left");
+                  $(this).removeClass("idle").addClass("active");
+                  });
+                });
+
+                $(function() {
+                  $(".btn-signup").click(function(e) {
+                    $(".nav").toggleClass("nav-up");
+                    $(".form-signup-left").toggleClass("form-signup-down");
+                    $(".success").toggleClass("success-left");
+                    $(".frame").toggleClass("frame-short");
+                  });
+                });
+
+                $(function() {
+                  $(".btn-signin").click(function() {
+                    $(".btn-animate").toggleClass("btn-animate-grow");
+                    $(".welcome").toggleClass("welcome-left");
+                    $(".cover-photo").toggleClass("cover-photo-down");
+                    $(".frame").toggleClass("frame-short");
+                    $(".profile-photo").toggleClass("profile-photo-down");
+                    $(".btn-goback").toggleClass("btn-goback-up");
+                    $(".forgot").toggleClass("forgot-fade");
+                  });
+                });
+        }
 
   }
